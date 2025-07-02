be3f7f50821bd2a2cc284fb118b890cb2d6d5a65
Shashankshekharjha7 <shashankshekharjha7@gmail.com>
2025-07-02 22:27:11 +0530

    2-count-odd-numbers-in-an-interval-range completed

---
+++ 2-count-odd-numbers-in-an-interval-range/2-count-odd-numbers-in-an-interval-range.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int countOdds(int low, int high) {
+      int count = 0;
+      for (int i = low; i <=high; i++)
+      {
+        if (i%2!=0){
+          count++;
+        }
+      }
+      return count;
+      
+
+        
+    }
+};
