d015c96eb118aafed616f1354cd90f9fb837b05f
Shashankshekharjha7 <shashankshekharjha7@gmail.com>
2025-07-03 13:11:54 +0530

    3-number-of-steps-to-reduce-a-number-to-zero completed

---
+++ 3-number-of-steps-to-reduce-a-number-to-zero/3-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    int numberOfSteps(int num) {
+
+      int steps = 0;
+      while (num>0)
+      {
+        if (num%2==0)
+        {
+          num /=2;
+        }
+        else
+        {
+          num --;
+        }
+        
+        steps++;
+      }
+      return steps;
+        
+    }
+};
